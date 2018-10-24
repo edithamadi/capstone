6b71bbc9070184a75bcb91a8404b7d52330d549f
Edith <you@example.com>
2018-10-24 19:48:46 +0300

    w

--- bimaapp/models.py
+++ bimaapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    bio = models.CharField(max_length=200)
+    profile_photo = models.ImageField(upload_to='profile/')
+    id_number = models.IntegerField()
+    maritalstatus = models.CharField(max_length=30)
+    cellphone = models.IntegerField()
+    emailaddress =  models.EmailField(max_length=75)
+
